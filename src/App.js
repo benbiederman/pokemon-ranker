28358210aee8a1d863be6a49cddb053c0278fb61
Ben Biederman <benbiederman@gmail.com>
2022-05-04 20:01:33 -0500

    Add functionality to navigation.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Gen1 from "../src/pages/Generations/Gen1";
+import Gen2 from "../src/pages/Generations/Gen2";
+import Gen3 from "../src/pages/Generations/Gen3";
+import Gen4 from "../src/pages/Generations/Gen4";
+import Gen5 from "../src/pages/Generations/Gen5";
+import Gen6 from "../src/pages/Generations/Gen6";
+import Gen7 from "../src/pages/Generations/Gen7";
+import Gen8 from "../src/pages/Generations/Gen8";
 import Navigation from "./components/Navigation/Navigation";
 import "./style/App.scss";
 
 function App() {
   const [activePage, setActivePage] = useState("Gen 1");
+  const [navActive, setNavActive] = useState(false);
+
+  function toggleNav() {
+    setNavActive(!navActive);
+  }
+
+  function updateActivePage(e) {
+    setActivePage(e.target.text);
+    toggleNav();
+  }
 
   return (
-    <div className="app">
-      <Navigation />
-    </div>
+    <Router>
+      <div className="app">
+        <Navigation
+          navActive={navActive}
+          toggleNav={toggleNav}
+          updateActivePage={updateActivePage}
+        />
+        <Routes>
+          <Route exact path="/" element={<Gen1 />} />
+          <Route path="/Gen2" element={<Gen2 />} />
+          <Route path="/Gen3" element={<Gen3 />} />
+          <Route path="/Gen4" element={<Gen4 />} />
+          <Route path="/Gen5" element={<Gen5 />} />
+          <Route path="/Gen6" element={<Gen6 />} />
+          <Route path="/Gen7" element={<Gen7 />} />
+          <Route path="/Gen8" element={<Gen8 />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,46 +7,53 @@ import tepig from "../../images/tepig.png";
 import froakie from "../../images/froakie.png";
 import litten from "../../images/litten.png";
 import grookey from "../../images/grookey.png";
+import { Link } from "react-router-dom";
 
-const Navigation = () => {
+const Navigation = ({ navActive, toggleNav, updateActivePage }) => {
   return (
-    <div className="navigation">
+    <div
+      className={
+        navActive
+          ? "navigation navigation-active"
+          : "navigation navigation-inactive"
+      }
+    >
       <div className="nav-items">
-        <div className="nav-item">
+        <Link to="/" className="nav-item" onClick={updateActivePage}>
           <img src={squirtle} alt="Gen 1 starter Squirtle" />
           <h3>Gen 1</h3>
-        </div>
-        <div className="nav-item">
+        </Link>
+        <Link to="/Gen2" className="nav-item" onClick={updateActivePage}>
           <img src={cyndaquil} alt="Gen 2 starter Cyndaquil" />
           <h3>Gen 2</h3>
-        </div>
-        <div className="nav-item">
+        </Link>
+        <Link to="/Gen3" className="nav-item" onClick={updateActivePage}>
           <img src={treecko} alt="Gen 3 starter Treecko" />
           <h3>Gen 3</h3>
-        </div>
-        <div className="nav-item">
+        </Link>
+        <Link to="/Gen4" className="nav-item" onClick={updateActivePage}>
           <img src={turtwig} alt="Gen 4 start Turtwig" />
           <h3>Gen 4</h3>
-        </div>
-        <div className="nav-item">
+        </Link>
+        <Link to="/Gen5" className="nav-item" onClick={updateActivePage}>
           <img src={tepig} alt="Gen 5 starter Tepig" />
           <h3>Gen 5</h3>
-        </div>
-        <div className="nav-item">
+        </Link>
+        <Link to="/Gen6" className="nav-item" onClick={updateActivePage}>
           <img src={froakie} alt="Gen 6 starter Froakie" />
           <h3>Gen 6</h3>
-        </div>
-        <div className="nav-item">
+        </Link>
+        <Link to="/Gen7" className="nav-item" onClick={updateActivePage}>
           <img src={litten} alt="Gen 7 starter Litten" />
           <h3>Gen 7</h3>
-        </div>
-        <div className="nav-item">
+        </Link>
+        <Link to="/Gen8" className="nav-item" onClick={updateActivePage}>
           <img src={grookey} alt="Gen 8 starter Grookey" />
           <h3>Gen 8</h3>
-        </div>
+        </Link>
       </div>
       <div className="nav-btn">
-        <button>+</button>
+        <button onClick={toggleNav}>+</button>
       </div>
     </div>
   );
